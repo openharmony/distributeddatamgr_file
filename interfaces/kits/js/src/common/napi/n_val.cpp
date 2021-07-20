f158af300456f5bd00210a0d4bbc3ce07e10bf59
maokelong <chenjinglong1@huawei.com>
2021-07-20 20:38:28 +0800

    Upload the implementation of @system.file

--- interfaces/kits/js/src/common/napi/n_val.cpp
+++ interfaces/kits/js/src/common/napi/n_val.cpp
@@ -198,6 +198,13 @@ NVal NVal::CreateInt64(napi_env env, int64_t val)
     return { env, res };
 }
 
+NVal NVal::CreateInt32(napi_env env, int32_t val)
+{
+    napi_value res = nullptr;
+    napi_create_int32(env, val, &res);
+    return { env, res };
+}
+
 NVal NVal::CreateObject(napi_env env)
 {
     napi_value res = nullptr;
