7490c924f34a8f5685b546821510beaf3947df62
wangjianqiang <wangjianqiang19@huawei.com>
2022-03-17 15:57:42 +0800

    add question
    Signed-off-by: wangjianqiang <wangjianqiang19@huawei.com>

--- interfaces/kits/js/src/mod_fileio/properties/read_text.cpp
+++ interfaces/kits/js/src/mod_fileio/properties/read_text.cpp
@@ -112,7 +112,8 @@ napi_value ReadText::Sync(napi_env env, napi_callback_info info)
         UniError(EINVAL).ThrowErr(env, "file is too large");
         return nullptr;
     }
-    memset_s(readbuf.get(), len + 1, 0, len + 1);
+    size_t lenght { len + 1 };
+    memset_s(readbuf.get(), lenght, 0, lenght);
     if (position > 0) {
         ret = pread(sfd.GetFD(), readbuf.get(), len, position);
     } else {
