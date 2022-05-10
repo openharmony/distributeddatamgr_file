c9e6af3176fba5a7d553673ce7e21deaebf85dbc
haonan_7 <haonan2@huawei.com>
2022-05-10 11:03:33 +0800

    add Sync functions
    Signed-off-by: haonan_7 <haonan2@huawei.com>

--- interfaces/kits/js/src/mod_securitylabel/securitylabel_n_exporter.cpp
+++ interfaces/kits/js/src/mod_securitylabel/securitylabel_n_exporter.cpp
@@ -107,7 +107,7 @@ napi_value SetSecurityLabelSync(napi_env env, napi_callback_info info)
         UniError(EINVAL).ThrowErr(env, "Invalid dataLevel");
         return nullptr;
     }
-    std::bool ret;
+    bool ret;
     ret = SecurityLabel::SetSecurityLabel(path.get(), dataLevel.get());
     return NVal::CreateUndefined(env).val_;
 }
